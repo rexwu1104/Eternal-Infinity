901499a5262a6fae85ceb7c5dc6875de614714b7
rexwu1104 <44058270+rexwu1104@users.noreply.github.com>
2022-06-21 18:14:13 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,67 +1,124 @@
-import os
+try:
+    import discord
+except:
+    __import__('os').system('bash ./install_discord.py.sh')
+
+from Commands.Music import ModeConverter
+from config import *
+
 import asyncio
-import nextcord as nc
-from nextcord.ext import commands
-from itertools import cycle
-from discord_together import DiscordTogether
+import discord_together
 
-def is_owner(ctx: commands.Context) -> bool:
-	return ctx.author.id == 606472364271599621
+from typing import Optional, Union
+from discord import Intents, Interaction, Object, StageChannel, VoiceChannel
+from discord.ext import commands
+from discord.app_commands import CommandTree
 
-intents = nc.Intents.all()
-bot = commands.AutoShardedBot(
-	command_prefix = 'ei!',
-	intents = intents
-)
+bot = commands.AutoShardedBot(application_id=928250356494913567, command_prefix="t!", intents=Intents.all())
+bot._connection._command_tree = None
+Applications = CommandTree(bot)
 
-@bot.event
-async def on_ready():
-	bot.togetherControl = await DiscordTogether('NzIzNTQzODg4Nzg2MzU4MzA0.XuzKwQ.ZgZaLqOvXLvlp0fmFQzLviZafOw')
-	
-	for fin in os.listdir('./cmds'):
-		if fin.endswith('.py'):
-			bot.load_extension(f'cmds.{fin[:-3]}')
-		elif os.path.isdir(f'./cmds/{fin}'):
-			bot.load_extension(f'cmds.{fin}')
-			
-	print('EI start online.')
-
-@bot.command()
-@commands.check(is_owner)
-async def load(ctx: commands.Context, file_name: str):
-	bot.load_extension(f'cmds.{file_name}')
-	await ctx.message.delete()
-
-@bot.command()
-@commands.check(is_owner)
-async def unload(ctx: commands.Context, file_name: str):
-	bot.unload_extension(f'cmds.{file_name}')
-	await ctx.message.delete()
-
-@bot.command()
-@commands.check(is_owner)
-async def reload(ctx: commands.Context, file_name: str):
-	bot.reload_extension(f'cmds.{file_name}')
-	await ctx.message.delete()
- 
-@bot.command()
-@commands.check(is_owner)
-async def guilds(ctx: commands.Context):
-    await ctx.message.delete()
-    async for guild in bot.fetch_guilds(limit=None):
-        try:
-            invites = await guild.invites()
-        except:
-            continue
+# @bot.event
+# async def on_ready():
+#     await bot.tree.sync(guild=Object(id=723162132082065501))
+    
+# @bot.tree.command()
+# async def play(interaction: Interaction, url: str):
+#     context: commands.Context = await bot.get_context(interaction.message)
         
-        print(guild.name, invites[0].url if len(invites) >= 1 else "No link")
+#     await context.invoke(bot.cogs['Music'].play, url=url)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def search(interaction: Interaction, query: str):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].search, query=query)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def join(interaction: Interaction, channel: Optional[Union[VoiceChannel, StageChannel]] = None):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].join, channel=channel)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def leave(interaction: Interaction):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].leave)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def loop(interaction: Interaction, rang: str = None):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].loop, rang=await ModeConverter.convert(rang))
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def shuffle(interaction: Interaction):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].shuffle)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def skip(interaction: Interaction, position: int = None):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].skip, position=position)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def pause(interaction: Interaction):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].pause)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def volume(interaction: Interaction, volume: float):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].volume, vol=volume)
+#     await interaction.response.defer()
 
-@bot.event
-async def on_command_error(ctx: commands.Context, err: Exception):
-   print(err.__dict__)
-   err = err.args[0].split(': ')[-1].strip(' ')
-   msg = await ctx.send('**' + err + '**')
-   await msg.delete(delay=5)
+# @bot.tree.command()
+# async def nowinfo(interaction: Interaction):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].nowinfo)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def queue(interaction: Interaction):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].queue)
+#     await interaction.response.defer()
 
-if __name__ == '__main__':
-	bot.run('NzIzNTQzODg4Nzg2MzU4MzA0.XuzKwQ.ZgZaLqOvXLvlp0fmFQzLviZafOw')
+# @bot.tree.command()
+# async def remove(interaction: Interaction, position: int):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].remove, position=position)
+#     await interaction.response.defer()
+    
+# @bot.tree.command()
+# async def together(interaction: Interaction):
+#     context: commands.Context = await bot.get_context(interaction.message)
+        
+#     await context.invoke(bot.cogs['Music'].together)
+#     await interaction.response.defer()
+
+async def main():
+    await bot.load_extension('Commands')
+    async with bot:
+        bot.togetherControl = await discord_together.DiscordTogether(token)
+        
+        await bot.start(token)
+        print('new version!!!')
+    
+asyncio.run(main())
